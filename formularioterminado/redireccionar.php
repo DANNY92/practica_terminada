7dd139f6478600f81a2a0d31a5c87b6b8f8e8d35
DANNY92 <danny_villarruelp@hotmail.com>
2013-10-28 12:06:23 -0600

    practica agenda terminada

---
+++ formularioterminado/redireccionar.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if(!isset($_SESSION['usuario'])){
+	header('location: ingresar.php');
+}
+echo "BIENVENIDO ".$_SESSION['usuario']."<br/>";
+?>
+<form action="nuevo.php" method="POST"> 
+<a href="nuevo.php">Nuevo</a>
+<a href="datos.php">Cerrar sesion</a>
+<br>
+<br>
+<br>
+<br>
+<?php
+include('conexion.php');
+$Con = new conexion();
+$Con->tablaDatos();
+?>
